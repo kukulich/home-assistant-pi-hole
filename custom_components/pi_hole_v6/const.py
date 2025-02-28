7affa20255c5fe7b62768b21fa32e7a9d9f9c512
Bastien Gautier <contact@bastiengautier.com>
2025-02-28 00:49:04 +0000

    Remove password :)

--- custom_components/pi_hole_v6/const.py
+++ custom_components/pi_hole_v6/const.py
@@ -4,9 +4,8 @@ from datetime import timedelta
 
 DOMAIN = "pi_hole_v6"
 DEFAULT_NAME = "Pi-Hole"
-# DEFAULT_URL = "https://pi.hole:443/api"
-DEFAULT_URL = "http://nipogi.local:8088/api"
-DEFAULT_PASSWORD = "+lOoe/SvjmjzM4LGOzRynRIF69AvqyR2719BYV8rWhw="
+DEFAULT_URL = "https://pi.hole:443/api"
+DEFAULT_PASSWORD = "xxxxx"
 
 SERVICE_DISABLE = "disable"
 SERVICE_DISABLE_ATTR_DURATION = "duration"
